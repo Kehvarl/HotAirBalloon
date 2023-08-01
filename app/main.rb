e0fd0419960f4881329fa61554be9af3a8863371
Kehvarl <kehvarl@kehvarl.com>
2023-07-31 17:57:28 -0700

    Work out states
    Draw a background

--- app/main.rb
+++ app/main.rb
@@ -1,18 +1,22 @@
+def init args
+  args.state.gamestate = :mainmenu
+end
+
+
 def tick args
-  args.outputs.labels  << [640, 540, 'Hello World!', 5, 1]
-  args.outputs.labels  << [640, 500, 'Docs located at ./docs/docs.html and 100+ samples located under ./samples', 5, 1]
-  args.outputs.labels  << [640, 460, 'Join the Discord server! https://discord.dragonruby.org', 5, 1]
+  if args.state.tick_count == 0
+    init args
+  end
 
-  args.outputs.sprites << { x: 576,
-                            y: 280,
-                            w: 128,
-                            h: 101,
-                            path: 'dragonruby.png',
-                            angle: args.state.tick_count }
+  case args.state.gamestate
+  when :mainmenu
+      mainmenu_tick(args)
+  else
+    puts "Invalid State"
+    args.state.gamestate = :mainmenu
+  end
+end
 
-  args.outputs.labels  << { x: 640,
-                            y: 60,
-                            text: './mygame/app/main.rb',
-                            size_enum: 5,
-                            alignment_enum: 1 }
+def mainmenu_tick args
+  args.outputs.solids << {x:0, y:0, w:1280, h:720, r:0, g:0, b:128}
 end
