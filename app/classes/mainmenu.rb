b62497ce790427b02af8aaf2f00a840133f2bfdd
Kehvarl <kehvarl@kehvarl.com>
2023-08-03 19:56:43 -0700

    Expand Menu class

--- app/classes/mainmenu.rb
+++ app/classes/mainmenu.rb
@@ -1,5 +1,5 @@
 class Menu
-  attr_accessor :x, :y, :w, :h, :r, :g, :b, :a, :anchor_x, :anchor_y, :blendmode_enum
+  attr_accessor :x, :y, :w, :h, :r, :g, :b, :a, :anchor_x, :anchor_y, :blendmode_enum, :menu_items
 
   def initialize
     @x = 320
@@ -9,19 +9,6 @@ class Menu
     @r = 0
     @g = 0
     @b = 32
-  end
-
-  def draw
-    [
-      {x:@x, y:@y, w:@w, h:@h, r:@r, g:@g, b:@b}.solid!
-    ]
-  end
-end
-
-class MainMenu < Menu
-
-  def initialize
-    super
     @menu_items = ["New", "Sample", "Item"]
     @selected_item = 0
   end
@@ -39,12 +26,29 @@ class MainMenu < Menu
   end
 
   def draw
-    menu = super
+    menu = [
+      {x:@x, y:@y, w:@w, h:@h, r:@r, g:@g, b:@b}.solid!
+    ]
+
     @menu_items.each_with_index do |item, index|
       menu << {x:328, y:532-(40*index), w:640, h:40,  text:item, r:255, g:255, b: 255}.label!
     end
-
     menu << {x:320, y:500-40*@selected_item, w:640, h:40, b:64, a:215}.solid!
+
+    menu
+  end
+end
+
+class MainMenu < Menu
+
+  def initialize
+    super
+  end
+
+
+  def draw
+    menu = super
+
     menu
   end
 end
