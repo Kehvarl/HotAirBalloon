b33b3e73b7600dd82359e4d3286f82511c9560c6
Kehvarl <kehvarl@kehvarl.com>
2023-09-08 20:36:41 -0700

    Make and move mountains

--- app/classes/background.rb
+++ app/classes/background.rb
@@ -10,28 +10,25 @@ end
 class Playfield < Background
   def initialize
     super
+    @sky = {x:0, y:0, w:1280, h:720, r:0, g:0, b:192}.solid!
+    @ground = {x:0, y:0, w:1280, h:64, r:96, g:64, b:0}.solid!
+    @mountain_x = 1000
   end
 
   def draw
-     out = [
-       {x:0, y:0, w:1280, h:620, r:0, g:0, b:32}.solid!,
-       {x:0, y:0, w:1280, h:620, r:0, g:0, b:64}.solid!,
-       {x:0, y:0, w:1280, h:520, r:0, g:0, b:128}.solid!,
-       {x:0, y:0, w:1280, h:420, r:0, g:0, b:192}.solid!,
-       {x:0, y:0, w:1280, h:320, r:0, g:0, b:255}.solid!
-     ]
-     out << make_triangle(430, 0, 850, 0, 640, 210, 64, 64, 64, 255)
+     out = [@sky, @ground]
+     out << make_mountain(@mountain_x, 0, 240, 320, 64, 64, 64)
+     @mountain_x -= 1
      out
   end
 
-  def make_triangle *opts
-  x, y, x2, y2, x3, y3, r, g, b, a = opts
+  def make_mountain x, y, w, h, r, g, b
   {
-    x: x, y: y, x2: x2, y2: y2, x3: x3, y3: y3,
+    x: x, y: y, x2: x+w, y2: y, x3: x+(w/2), y3: y+h,
     r: r || 0,
     g: g || 0,
     b: b || 0,
-    a: a || 255
+    a: 255
   }.solid!
 end
 end
